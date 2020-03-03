b76b4ec6731cf7595468625f4ad97626f4543ae8
kelvionald <31734063+kelvionald@users.noreply.github.com>
2020-03-03 17:53:29 +0300

    рабочая 3.1

--- 3.1.rabbit/3.1.rabbit/3.1.rabbit.cpp
+++ 3.1.rabbit/3.1.rabbit/3.1.rabbit.cpp
@@ -30,7 +30,82 @@ using namespace std;
 
 #define DEV true
 
-typedef long long int64;
+struct bigint
+{
+	static const long long BASE = 1e9;
+	static const long long SIZE = 4;
+
+	long long digits[SIZE];
+
+	bigint(long long val)
+	{
+		memset(digits, 0, sizeof(long long) * SIZE);
+		int i = 0;
+		while (val)
+		{
+			digits[i++] = val % BASE;
+			val /= BASE;
+		}
+	}
+
+	void operator+=(const bigint& other)
+	{
+		for (int i = 0; i < SIZE; i++)
+		{ //сначала сложим числа поразрядно,
+			digits[i] += other.digits[i]; //игнорируя переполнения
+		}
+
+		for (int i = 0; i < SIZE - 1; i++)
+		{ //а затем поочередно выполним переносы
+			if (digits[i] >= BASE)
+			{ //для каждого разряда
+				digits[i] -= BASE;
+				digits[i + 1]++;
+			}
+		}
+	}
+
+	friend ostream& operator<<(ostream& out, const bigint& num)
+	{
+		string result;
+
+		char buffer[10];
+
+		for (int i = bigint::SIZE - 1; i >= 0; i--)
+		{
+			sprintf(buffer, "%09d", num.digits[i]);
+			result += buffer;
+		}
+
+		int first_idx = result.find_first_not_of('0');
+		if (first_idx == string::npos)
+		{
+			out << "0";
+		}
+		else
+		{
+			out << result.substr(first_idx);
+		}
+
+		return out;
+	}
+
+	bool operator==(long long val)
+	{
+		int i = 0;
+		while (val)
+		{
+			if (digits[i++] != val % BASE)
+			{
+				return false;
+			}
+			val /= BASE;
+		}
+		return true;
+	}
+};
+
+typedef bigint int64;
 
 template <typename T>
 T min(T a, T b)
@@ -45,7 +120,11 @@ int64 rabbit(int N, int K, string s = "")
 	{
 		int lenArr = arr.size();
 		int offset = lenArr - min(K, lenArr);
-		int64 s = accumulate(arr.begin() + offset, arr.end(), 0);
+		int64 s = 0;
+		for (int i = offset; i < arr.size(); i++)
+		{
+			s += arr[i];
+		}
 		arr.push_back(s);
 	}
 	return arr[N];
@@ -53,13 +132,15 @@ int64 rabbit(int N, int K, string s = "")
 
 void tests()
 {
-	cout << (rabbit(30, 2) == 1346269) << endl;
+	cout << rabbit(30, 2) << " " << (rabbit(30, 2) == 1346269) << endl;
+	cout << rabbit(10, 10) << " " << (rabbit(10, 10) == 512) << endl;
+	cout << rabbit(100, 100) << endl; //633825300114114700748351602688
 }
 
 void program(ifstream& in, ofstream& out)
 {
-	int64 N;
-	int64 K;
+	int N;
+	int K;
 	in >> N;
 	in >> K;
 	out << rabbit(N, K);
